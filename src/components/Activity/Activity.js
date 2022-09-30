12b3e988626a4dc11cf52e7714bd44c99fe44681
Showrove Roy <showrove200@gmail.com>
2022-09-30 14:17:37 +0600

    card data addeded

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import Card from '../Card/Card';
-import Header from '../Header/Header';
-import './Activity.css';
+import React, { useEffect, useState } from "react";
+import Card from "../Card/Card";
+import Header from "../Header/Header";
+import "./Activity.css";
 
 const Activity = () => {
-    const [activities, setActivities] = useState([]);
-    useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setActivities(data))
-            .catch(err => console.log(err));
-    }, []);
-    return (
-        <div className='activityContainer'> 
-            <Header></Header>
-            <h2>Select today’s exercise</h2>
-            {
-                activities.map(activity =>  <Card key={activity.id} activity={activity}></Card>)
-            }
-           
-        </div>
-    );
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setActivities(data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className='activityContainer'>
+      <Header></Header>
+      <h2>Select today’s exercise</h2>
+      <div className="mainCardContainer">
+        {activities.map((activity) => (
+          <Card key={activity.id} activity={activity}></Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Activity;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,15 @@ const Card = (props) => {
     // console.log(props.activity);
     return (
         <div className='cardContainer'>
+            <div className='card'>
+                <img src={img} alt="" />
+                <h4>{name}</h4>
+                <p>{details}</p>
+                <h6>For Age: {forAge}</h6>
+                <h6>Time required: {time}</h6>
+                <button>Add to list</button>
 
+            </div>
         </div>
     );
 };
