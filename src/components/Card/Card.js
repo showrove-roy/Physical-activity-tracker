ca1e7a50dc9c393bbf3359e9ec328cf0b6d284f9
Showrove Roy <showrove200@gmail.com>
2022-09-30 13:55:21 +0600

    data load complate

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
+    const {name,id,img,forAge,time,details} = props.activity;
+    // console.log(props.activity);
     return (
         <div className='cardContainer'>
-            Card
+
         </div>
     );
 };
